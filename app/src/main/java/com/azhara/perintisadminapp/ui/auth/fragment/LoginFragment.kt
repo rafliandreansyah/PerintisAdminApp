ae7d2ed38f4d1b72ffd715fbd39eaff72a9abebd
Rafli Andreansyah <rafliandreansyah957@gmail.com>
2021-01-03 16:54:48 +0700

    Perubahan layout login dan penerapan viewbinding

--- app/src/main/java/com/azhara/perintisadminapp/ui/auth/fragment/LoginFragment.kt
+++ app/src/main/java/com/azhara/perintisadminapp/ui/auth/fragment/LoginFragment.kt
@@ -1,60 +1,63 @@
 package com.azhara.perintisadminapp.ui.auth.fragment
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.azhara.perintisadminapp.R
+import com.azhara.perintisadminapp.databinding.FragmentLoginBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+class LoginFragment : Fragment(), View.OnClickListener {
 
-/**
- * A simple [Fragment] subclass.
- * Use the [LoginFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class LoginFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private var _binding: FragmentLoginBinding? = null
+    private val binding get() = _binding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+
+        _binding = FragmentLoginBinding.inflate(inflater, container, false)
+        return binding?.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment LoginFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            LoginFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding?.btnLogin?.setOnClickListener(this)
+        binding?.btnResetPassword?.setOnClickListener(this)
+
+        textWatcher()
+    }
+
+    private fun textWatcher(){
+        val watcher = object : TextWatcher{
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if (s?.length == 0){
+                    binding?.btnLogin?.isEnabled = false
+                }else{
+                    binding?.btnLogin?.isEnabled = binding?.edtEmailLogin?.text.toString().trim() != "" && binding?.edtPasswordLogin?.text.toString().trim() != ""
                 }
             }
+
+            override fun afterTextChanged(s: Editable?) {
+            }
+
+        }
+
+        binding?.edtEmailLogin?.addTextChangedListener(watcher)
+        binding?.edtPasswordLogin?.addTextChangedListener(watcher)
     }
+
+    override fun onClick(v: View?) {
+        when(v?.id){
+            
+        }
+    }
+
 }
